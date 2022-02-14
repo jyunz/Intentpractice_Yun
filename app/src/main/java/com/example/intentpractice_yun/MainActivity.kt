325afd10e8026ba6bc1654f334ef2a9a1733e990
Roy <J.jyun0525@gmail.com>
2022-02-14 14:31:14 +0900

    VIEW액션 예제 -인터넷 링크 -플레이스토어 상세화면 링크

--- app/src/main/java/com/example/intentpractice_yun/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_yun/MainActivity.kt
@@ -14,6 +14,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        kakaoStoreBtn.setOnClickListener {
+
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW,myUri)
+
+            startActivity(myIntent)
+        }
+
+        naverBtn.setOnClickListener {
+            val myUri = Uri.parse("http://naver.com")
+            val myIntent = Intent(Intent.ACTION_VIEW,myUri)
+
+            startActivity(myIntent)
+        }
+
+
         smsBtn.setOnClickListener {
             val inputPhoneNum = phoneNumEdt.text.toString()
             val myUri = Uri.parse("smsto:${inputPhoneNum}")
